82b3fe585a35e8c60a457ebc8b74b51091ca6ea7
InezaBonheurCedrick <172363567+InezaBonheurCedrick@users.noreply.github.com>
2025-10-30 09:40:27 +0200

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -18,7 +18,7 @@ dotenv.config();
 const app: Application = express();
 const prisma = new PrismaClient();
 
-app.use(cors({ origin: "https://house-major-website.vercel.app/", credentials: true }));
+app.use(cors({ origin: ["http://localhost:5173", "https://house-major-website.vercel.app"], credentials: true }));
 app.use(helmet());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
