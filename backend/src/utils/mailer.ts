dd897bb110b481c2cc4a899518f726a8ca804479
InezaBonheurCedrick <172363567+InezaBonheurCedrick@users.noreply.github.com>
2025-10-31 11:21:41 +0200

    Update mailer.ts

--- backend/src/utils/mailer.ts
+++ backend/src/utils/mailer.ts
@@ -1,8 +1,8 @@
 import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com", // Gmail's SMTP server
-  port: 465,              // Port for SSL
+  host: "smtp.gmail.com", 
+  port: 465,              
   secure: true,
   auth: {
     user: process.env.SMTP_USER,
