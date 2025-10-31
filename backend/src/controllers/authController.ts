230197bf88c733441d002e414d1a6b71e3cf07b6
InezaBonheurCedrick <172363567+InezaBonheurCedrick@users.noreply.github.com>
2025-10-31 11:38:33 +0200

    added tls

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -15,7 +15,10 @@ const transporter = nodemailer.createTransport({
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
-  }
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
 
 @Route("auth")
