ffb04fb79d5b2b094945b9868650272dbf57021e
InezaBonheurCedrick <172363567+InezaBonheurCedrick@users.noreply.github.com>
2025-10-31 11:13:41 +0200

    Update mailer.ts

--- backend/src/utils/mailer.ts
+++ backend/src/utils/mailer.ts
@@ -7,7 +7,7 @@ const transporter = nodemailer.createTransport({
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
-  },
+  }, 
 });
 
 export const sendContactEmail = async (contact: {
