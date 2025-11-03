f0deaeb88e59b07790571308280fa4f5c5cab4aa
InezaBonheurCedrick <172363567+InezaBonheurCedrick@users.noreply.github.com>
2025-11-03 15:15:48 +0200

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -93,7 +93,7 @@ const Header = ({ isDark, toggleTheme }) => {
     else {
       navigate(href);
       setIsSidebarOpen(false);
-      setActiveSection('');//this is bad
+      setActiveSection('');
     }
   };
 
